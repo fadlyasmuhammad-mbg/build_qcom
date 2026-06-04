f1e6e10a03b54f56c398f98fc692ef446e2b770f
fadlyasmuhammad-mbg <fadlyasmuhammad@gmail.com>
2026-06-04 08:18:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,24 @@
 
 git clone --depth=1 \
   -b qcom_sm8250 \
-  https://github.com/bengal-upstream/kernel_xiaomi_sm8250.git \
+  https://github.com/bengal-upstream/kernel_xiaomi_sm8250 \
   kernel
   
-git clone --depth=1 \
-  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 \
-  clang
-  
-CLANG_DIR=$(find clang -maxdepth 1 -type d -name "clang-r*" | sort -V | tail -n1)
+# setup clang
+clang_dir="$(pwd)/clang"
 
-export PATH="$(pwd)/$CLANG_DIR/bin:$PATH"
+mkdir -p "$clang_dir"
 
+clang_url="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/9c58a6fe9b3a84143a3ebd54f9e59328be769f5a/clang-r596125.tar.gz"
+
+echo "Downloading AOSP Clang..."
+
+curl -L "$clang_url" | tar -xz -C "$clang_dir"
+
+# detect folder hasil extract
+export CLANG_DIR=$(find "$clang_dir" -maxdepth 1 -type d -name "clang*" | head -n1)
+
+export PATH="$CLANG_DIR/bin:$PATH"
 export BUILD_ARGS="LLVM=1 LLVM_IAS=1"
 
 echo "=== Compiler ==="
